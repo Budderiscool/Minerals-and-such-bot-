d2b144a587a867e8f56c1ff389021dec0ab6493a
Budder <austin.joseph.wallin@gmail.com>
2025-12-07 11:22:04 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,14 @@ import { importKeyRaw, discordInteraction } from "./deps.ts";
 import { commands } from "./commands.ts";
 
 const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY");
-if (!PUBLIC_KEY) {
-  console.error("ERROR: DISCORD_PUBLIC_KEY not set.");
-  Deno.exit(1);
-}
+if (!PUBLIC_KEY) throw new Error("DISCORD_PUBLIC_KEY not set");
 
 const key = await importKeyRaw(PUBLIC_KEY);
 
-const handler = await discordInteraction(key, commands);
-
-addEventListener("fetch", (e) => {
-  e.respondWith(handler(e.request));
+// Correct usage: pass commands inside an object
+const handler = await discordInteraction({
+  key,
+  commands,
 });
+
+addEventListener("fetch", (e) => e.respondWith(handler(e.request)));
