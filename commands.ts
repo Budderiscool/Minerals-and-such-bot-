ba09d45a0a4fc379ccfce3ec8cb72928e113923a
Budder <austin.joseph.wallin@gmail.com>
2025-12-07 11:45:50 -0600

    Delete commands.ts

--- commands.ts
+++
@@ -1,9 +0,0 @@
-// commands.ts
-export const commands: Record<string, { run: (interaction: any) => string }> = {
-  ping: {
-    run: () => "Pong!",
-  },
-  help: {
-    run: () => "Available commands: /ping, /help",
-  },
-};
